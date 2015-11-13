e651f78cd0074abe380fdd5c15d0a4e58210a39d
Carmina Perez Guerrero <carminita_perez@hotmail.com>
2015-11-13 00:33:07 -0600

    Update WSQ11.py

--- WSQ11.py
+++ WSQ11.py
@@ -9,15 +9,17 @@ def Lychrel(a,b):
     for i in candidate:
         if i == reverse(i):
             palindr += 1
+        elif i[-1] == 0:
+            palindr += 1
         else:
             q=i
-            while q != reverse(q) and count <30:
-                q+=reverse(q)
-                count += 1
-            if count > 30:
-                lych += 1
-            else:
-                nonlych += 1
+            while q != reverse(q) and count != 30:
+                q = q+reverse(q)
+                if q == reverse(q):
+                    nonlych += 1
+                count +=1
+            if q!=reverse(q) and count >= 30:
+                lych+=1
     print(("Natural Palindromes: ")+str(palindr))
     print(("Non-Lychrels (become palindrome): ")+str(nonlych))
     print(("Lychrel candidates: ")+str(lych))
