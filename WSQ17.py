45f9da92b8bc98c9e754ac97e99a38af26f7ab82
Carmina Perez Guerrero <carminita_perez@hotmail.com>
2015-12-01 07:30:03 -0600

    Update WSQ17.py
    I was finally able to make the movie titles into keys and the actors a list of values for each movie

--- WSQ17.py
+++ WSQ17.py
@@ -1,13 +1,12 @@
 with open('movies.txt', 'r') as document:
-    movie_dict = {}
+    dictionary = {}
     for line in document:
         line = line.strip("\r\n")
         line = line.split(", ")
-        for i in range(0,(len(line)-1)):
-            if line[i+1] not in movie_dict:
-                movie_dict[line[i+1]] = line[0]
-            elif line[i+1] in movie_dict:
-                key = line[i+1]
-                movie_dict.setdefault(key, [])
-                movie_dict[key].append(line[0])
-        print (movie_dict)
+        for i in range (1, (len(line)-1)):
+            movie = line[i]
+            actor = line[0]
+            if movie not in dictionary:
+                dictionary[movie] = [actor]
+            else:
+                dictionary[movie].append(actor)
